3c8781b6e66b3662c46089ac73680f4a92a27a0f
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2026-03-19 08:34:34 +0400

    [TCMS-12] .env.sh register TEST_PLAN_DOC_GEN

--- .env.sh
+++ .env.sh
@@ -1,1 +1,5 @@
 [[ -f "Makefile" ]] && [[ 0 -eq $(grep -q "test:" Makefile) ]]
+
+if [[ -d $HOME/Documents/projects/test-plan-documentation-generator ]]; then
+	export TEST_PLAN_DOC_GEN="$HOME/Documents/projects/test-plan-documentation-generator/"
+fi
