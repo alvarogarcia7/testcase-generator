2bd62d9f197bdbed18b39933f6d5c85bb784d5e8
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2026-05-14 15:45:22 +0400

    force github email

--- .env.sh
+++ .env.sh
@@ -4,3 +4,7 @@ if [[ -d $HOME/Documents/projects/test-plan-documentation-generator ]]; then
 	export TEST_PLAN_DOC_GEN="$HOME/Documents/projects/test-plan-documentation-generator/"
 	export TEST_PLAN_DOC_GEN_DIR="$HOME/Documents/projects/test-plan-documentation-generator/"
 fi
+
+export GIT_AUTHOR_EMAIL="alvarogarcia7@users.noreply.github.com"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
